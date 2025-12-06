cb98f483fe635c6b7fe4f3f306d32f77a1ed2734
JHOJAN <232162@unamba.edu.pe>
2025-12-05 19:15:17 -0500

    Versión 2.0.0 - Menú básico del sistema

--- src/main.java
+++ src/main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class main {
     public static void main(String[] args) {
-        System.out.println("Sistema de Biblioteca - Versión 1.1.0 Mejorada");
+        Scanner sc = new Scanner(System.in);
+        int opcion;
+
+        do {
+            System.out.println("==== SISTEMA DE BIBLIOTECA v2.0.0 ====");
+            System.out.println("1. Registrar Libro");
+            System.out.println("2. Registrar Socio");
+            System.out.println("3. Registrar Préstamo");
+            System.out.println("0. Salir");
+            System.out.print("Seleccione una opción: ");
+            opcion = sc.nextInt();
+
+        } while (opcion != 0);
+
+        System.out.println("Sistema finalizado.");
     }
 }
